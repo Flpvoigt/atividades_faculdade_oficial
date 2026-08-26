d1efe48aa6b3ddc6e41a6f47150b45701880b639
Flpvoigt <flpvoigt@gmail.com>
2026-08-26 17:49:03 -0300

    primeiras resoluções das condicionais

---
+++ Condicional/comparation_temps.py
@@ -0,0 +1,13 @@
+x = float(input("Digite uma temperatura: "))
+
+if x < 10:
+    print("frio")
+
+elif x >= 10 and x <=25:
+    print("normal")
+
+elif x > 25 and x < 35:
+    print("Quente")
+
+else:
+    print("indaial")
---
+++ Condicional/seletion_student.py
@@ -0,0 +1,9 @@
+x = float(input("Digite a primeira nota: "))
+y = float(input("Digite a segunda nota: "))
+
+z = (x + y) / 2
+
+if z >= 7:
+    print("Aprovado")
+else:
+    print("reprovado")
